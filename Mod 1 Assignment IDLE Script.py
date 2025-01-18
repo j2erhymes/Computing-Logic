8a85188ad775f2142c5aaca7a34fb37670d7dbd7
j2erhymes <j2erhymes@gmail.com>
2025-01-17 23:18:42 -0500

    Add files via upload

---
+++ Mod 1 Assignment IDLE Script.py
@@ -0,0 +1,11 @@
+STRING_CATS = "cats"
+
+STRING_DOGS = "dogs"
+
+finalStatement = print
+
+STRING_CATS = input("It is raining cats and dogs.")
+
+STRING_DOGS = input("It is raining cats and dogs.")
+
+print
